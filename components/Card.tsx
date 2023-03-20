72406ef715dc410df48fcb6ebd6fb5c4f7ce1d25
AntonLan <lanqel@gmail.com>
2023-03-20 14:34:58 +0700

    refactoring style organization card

--- components/Card.tsx
+++ components/Card.tsx
@@ -8,7 +8,7 @@ interface CardProps {
 const Card: FC<CardProps> = ({organization}) => {
 
 	return (
-			<div className="block max-w-[200px] m-2.5 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+			<div className="block cursor-pointer w-[200px] h-[200px] m-2.5 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
 				<h5 className="mb-2 font-bold tracking-tight text-gray-900 dark:text-white">{organization.name}</h5>
 				<p className="font-normal text-gray-700 dark:text-gray-400">{organization.formOrganization}</p>
 				<p className="font-normal text-gray-700 dark:text-gray-400">{organization.character}</p>
