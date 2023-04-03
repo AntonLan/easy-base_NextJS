7ee141c8e3a4945077a590356c0d5422b79ebb9f
AntonLan <lanqel@gmail.com>
2023-04-03 13:42:15 +0700

    add dark for table and sidebar

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,12 @@ module.exports = {
 				'primary': '#379BA6',
 				'txt-color': '#39BFBF',
 				'light-green': '#39BFB2',
-				'light-gray': '#F2F2F2'
+				'light-gray': '#F2F2F2',
+				'dark-light-gray': '#595959',
+				'dark-light-green': '#1B5953',
+				'dark-txt-color': '#c8fdfd',
+				'dark-primary': '#266B73',
+				'dark-main': '#011116',
 			}
 		}
 	},
